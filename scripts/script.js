fcaf106a33dd8d01a1df4c8c59a890d29bbf341a
lawlug026 <ujwalgoyal.2@gmail.com>
2018-04-22 15:18:48 +0530

    setup website

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+document.getElementById('course-menu').onmouseover = () => {
+	document.getElementById('course-menu-submenu').style.display = "block";
+}
+
+document.getElementById('course-menu').onmouseleave = () => {
+	document.getElementById('course-menu-submenu').style.display = "none";
+}
+
+document.getElementById('course-menu-submenu').onmouseover = () => {
+	document.getElementById('course-menu-submenu').style.display = "block";
+}
+
+document.getElementById('course-menu-submenu').onmouseleave = () => {
+	document.getElementById('course-menu-submenu').style.display = "none";
+}
+
+var showMe = () => {
+	console.log("clicked");
+	document.getElementById('mob-submenu').style.display = "block";
+	document.getElementById('mob').style.display = "none";
+	document.getElementById('mob1').style.display = "inline";
+}
+
+var hideMe = () => {
+	console.log("clicked");
+	document.getElementById('mob-submenu').style.display = "none";
+	document.getElementById('mob').style.display = "inline";
+	document.getElementById('mob1').style.display = "none";
+}
+
+var showMenu = () => {
+	document.getElementById('nav-mob').style.display = "block";
+	document.getElementById('menu-mob1').style.display = "inline";
+	document.getElementById('menu-mob').style.display = "none";
+}
+
+var hideMenu = () => {
+	document.getElementById('nav-mob').style.display = "none";
+	document.getElementById('menu-mob1').style.display = "none";
+	document.getElementById('menu-mob').style.display = "inline";
+}
